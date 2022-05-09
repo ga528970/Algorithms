bb428d96d9613041267db3197a5809c4b2b4b84b
ga528970 <99818248+ga528970@users.noreply.github.com>
2022-05-09 10:09:50 -0400

    Psudocode

---
+++ main.java
@@ -0,0 +1,37 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Welcome to The Family Tree Simulator!!!");
+
+/// Print tree diagram of the enitre family each person is numbered
+/// User will pick 2 people
+/// System will print how they are related
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+//void show(String text) {
+  //   System.out.println(text);
+   // }
+    
+  }
+}
